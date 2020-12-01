5945f2306af1b4a3884292316787086904ae9606
Aleksander Kus <alex.qs234@gmail.com>
2020-12-01 10:39:39 +0100

    added redshift to xmonad startup

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -5,5 +5,6 @@ picom &
 setxkbmap -option caps:escape
 nm-applet &
 volumeicon &
+redshift &
 trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x282c34  --height 22 &
 xsetroot -cursor_name left_ptr
