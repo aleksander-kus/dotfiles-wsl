0a5eeec5c8d85ff81308a6dbee3f72aaa8e882e7
Aleksander Kus <alex.qs234@gmail.com>
2020-12-06 09:59:15 +0100

    added autokey to startup, modified spaces and pcmanfm conf

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -7,4 +7,5 @@ nm-applet &
 volumeicon &
 redshift &
 trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x282c34  --height 22 &
+autokey-gtk &
 xsetroot -cursor_name left_ptr
