6f780e0192ea2f73ccf7e5339c605b93d027f107
Aleksander Kus <alex.qs234@gmail.com>
2020-12-17 09:56:09 +0100

    added some programs to autostart

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -9,4 +9,8 @@ redshift &
 trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x282c34  --height 22 &
 autokey-gtk &
 pavucontrol &
+dunst &
+DAY=$(date "+%u")
+[ $DAY != "6" -a $DAY != "7" ] && teams &
+steam &
 xsetroot -cursor_name left_ptr
