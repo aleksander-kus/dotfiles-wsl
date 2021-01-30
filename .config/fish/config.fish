443258650c79bef7b3fa5d279a9248d078545cca
Aleksander Kus <alex.qs234@gmail.com>
2021-01-30 15:24:28 +0100

    fixed annoying test bug in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -240,12 +240,14 @@ end
 ### END OF FUNCTIONS ###
 
 ### SSH AGENT ###
-if test -z (pgrep ssh-agent)
+pgrep ssh-agent 1>/dev/null 2>&1
+if test ! $status -eq 0
   eval (ssh-agent -c)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
+
 
 ### ALIASES ###
 # spark aliases
